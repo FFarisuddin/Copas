7ae2ac5bfa86a89425aeb29efa764df7da5e6c19
FFarisuddin <ffajar70@gmail.com>
2019-12-23 23:32:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,114 +1,56 @@
 <?php
-require_once 'vendor/autoload.php';
-require_once "./random_string.php";
+    require_once 'vendor/autoload.php';
+    require_once "./random_string.php";
 
-use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+    use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+    use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+    use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+    use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+    use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "DefaultEndpointsProtocol=http;AccountName=blobff;AccountKey=9SkV9J8qyevowLNw6rXH1eOSbfKnRYohlvOhEwWUHJZMiZP4AiD24smx/xkLRyLBg3+8c5PdjYzcemAP6Pf1EQ==";
-$containerName = "images";
-// Create blob client.
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-if (isset($_POST['submit'])) {
-	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
-	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-	// echo fread($content, filesize($fileToUpload));
-	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-	header("Location: index.php");
-}
-$listBlobsOptions = new ListBlobsOptions();
-$listBlobsOptions->setPrefix("");
-$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+    $connectionString = 'DefaultEndpointsProtocol=http;AccountName=blobff;AccountKey=9SkV9J8qyevowLNw6rXH1eOSbfKnRYohlvOhEwWUHJZMiZP4AiD24smx/xkLRyLBg3+8c5PdjYzcemAP6Pf1EQ==';
+    $containerName = "images";
+    // Create blob client.
+    $blobClient = BlobRestProxy::createBlobService($connectionString);
+    if (isset($_POST['submit'])) {
+        $fileToUpload = strtolower($_FILES["photo"]["name"]);
+        $content = fopen($_FILES["photo"]["tmp_name"], "r");
+        // echo fread($content, filesize($fileToUpload));
+        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+        header("Location: index.php");
+    }
+    $listBlobsOptions = new ListBlobsOptions();
+    $listBlobsOptions->setPrefix("");
+    $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 ?>
-
 <!DOCTYPE html>
 <html>
- <head>
- <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Analisis Gamabar</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
-  </head>
+<head>
+    <title>AGIT</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+</head>
 <body>
-	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-			<ul class="navbar-nav mr-auto">
-			
-			<li class="nav-item active">
-				<a class="nav-link" href="http://fajarfarisuddinweb.azurewebsites.net/index.php">Analisis<span class="sr-only">(current)</span></a>
-			</li>
-		</div>
-		</nav>
-		<main role="main" class="container">
-    		<div class="starter-template"> <br><br><br>
-        		<h1>Analisis Gambar</h1>
-				<p class="lead"><br> Click <b>Upload</b>, untuk menganlisa foto pilih <b>analyze</b> pada tabel.<br><b>Note</b>: Analisa gambar menggunakan API dari Computer Vision Azure<br>Untuk Penyimpanan menggunakan Azure Blob Storage<br><br><b>Azure For Students</b></p>
+    <h1>Cognitive Service x Blob Storage</h1>
 
-				<span class="border-top my-3"></span>
-			</div>
-		<div class="mt-4 mb-2">
-			<form class="d-flex justify-content-lefr" action="index.php" method="post" enctype="multipart/form-data">
-				<input type="file" name="fileToUpload" accept=".jpeg,.jpg,.png" required="">
-				<input type="submit" name="submit" value="Upload">
-			</form>
-		</div>
-		<br>
-		<br>
-		<h4>Total Files : <?php echo sizeof($result->getBlobs())?></h4>
-		<table class='table table-hover'>
-			<thead>
-				<tr>
-					<th>File Name</th>
-					<th>File URL</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php
-				do {
-					foreach ($result->getBlobs() as $blob)
-					{
-						?>
-						<tr>
-							<td><?php echo $blob->getName() ?></td>
-							<td><?php echo $blob->getUrl() ?></td>
-							<td>
-								<form action="computervision.php" method="post">
-									<input type="hidden" name="url" value="<?php echo $blob->getUrl()?>">
-									<input type="submit" name="submit" value="Analyze!" class="btn btn-primary">
-								</form>
-							</td>
-						</tr>
-						<?php
-					}
-					$listBlobsOptions->setContinuationToken($result->getContinuationToken());
-				} while($result->getContinuationToken());
-				?>
-			</tbody>
-		</table>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        Image to analyze: <input type="file" name="photo" accept=".jpeg,.jpg,.png" />
+        <input type="submit" name="Submit" value="Uploadd" />
+    </form> 
 
-	</div>
-
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
-    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
-  </body>
+    <br><br>
+    
+    <div id="wrapper" style="width:1020px; display:table;">
+        <div id="jsonOutput" style="width:600px; display:table-cell;">
+            Response:
+            <br><br>
+            <textarea id="responseTextArea" class="UIInput" style="width:580px; height:400px;"></textarea>
+        </div>
+    
+        <div id="imageDiv" style="width:420px; display:table-cell;">
+            Source image:
+            <br><br>
+            <img id="sourceImage" width="400" />
+        </div>
+    </div>
+</body>
 </html>
