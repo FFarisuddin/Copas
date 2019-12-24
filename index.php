b7c22b0a502963443e67d25c832b040fe999b14b
FFarisuddin <ffajar70@gmail.com>
2019-12-24 13:32:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>AGIT</title>
+    <title>Eftu Indonesia (F2)</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
