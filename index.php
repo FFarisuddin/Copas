25932099bf82d0040d450de60ee42ccf6f47a381
FFarisuddin <ffajar70@gmail.com>
2020-01-08 14:25:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
             "https://computer-visionn.cognitiveservices.azure.com/vision/v2.0/analyze";
  
         var params = {
-            "visualFeatures": "description.captions.text", 
+            "visualFeatures": "description.captions", 
         };
  
         var sourceImageUrl = document.getElementById("inputImage").value;
